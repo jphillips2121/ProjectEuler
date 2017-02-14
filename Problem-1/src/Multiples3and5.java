c7af9f74edbab68146e5d7ac79a9c97fe62c87d5
jphillips2121 <jphillips3@companieshouse.gov.uk>
2017-02-14 11:40:00 +0000

    Sums all integers between 1 and 1000 that are multiples of 3 or 5

---
+++ Problem-1/src/Multiples3and5.java
@@ -0,0 +1,14 @@
+/**
+ * Created by jphillips3 on 14/02/2017.
+ */
+public class Multiples3and5 {
+        public static void main(String[] args) {
+            int count = 0;
+            for (int i = 1; i < 1000; i++) {
+                if ((i % 3 == 0) || (i % 5 == 0)) {
+                    count += i;
+                }
+            }
+            System.out.println(count);
+        }
+}
