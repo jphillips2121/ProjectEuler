6d116d503a0ea740fa8dc91fd11a5ae93cd093f1
jphillips2121 <jphillips3@companieshouse.gov.uk>
2017-02-15 13:04:27 +0000

    The largest palindrome product of two 3 digit numbers multiplied together

---
+++ Problem-4/src/palindromeProd.java
@@ -0,0 +1,35 @@
+/**
+ * Created by jphillips3 on 15/02/2017.
+ */
+
+public class palindromeProd {
+    public static void main(String[] args) {
+        int maxNum = 0;
+        int iValue = 0;
+        int jValue = 0;
+        for (int i = 1; i < 1000; i++) {
+            for (int j = 1; j < 1000; j++) {
+                if ((isPalindrome(i*j) == true) & (i*j > maxNum)) {
+                    maxNum = i*j;
+                    iValue = i;
+                    jValue = j;
+                }
+            }
+        }
+        System.out.println("The largest palindrome product is " + iValue + " x " + jValue + " = " + maxNum);
+    }
+
+    //Checks if number is a palindrome or not
+    public static boolean isPalindrome(int num) {
+        String newNum = Integer.toString(num);
+        newNum = new StringBuilder(newNum).reverse().toString();
+        int newNumInt = Integer.parseInt(newNum);
+        if (newNumInt == num) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
