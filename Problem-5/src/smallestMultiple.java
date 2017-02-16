dbb5bd145d4669f7f636de5cbf15be5fba1734d3
jphillips2121 <jphillips3@companieshouse.gov.uk>
2017-02-16 10:27:12 +0000

    Calculates number divisible by all numbers between 1 and 20

--- Problem-5/src/smallestMultiple.java
+++ Problem-5/src/smallestMultiple.java
@@ -3,6 +3,22 @@
  */
 public class smallestMultiple {
     public static void main(String[] args) {
-
+        boolean multiplied = false;
+        int num = 0;
+        int multiples = 20;
+        while (!multiplied) {
+            num += 1;
+            int count = 0;
+            for (int i = 1; i <= multiples; i++) {
+                if (num % i == 0) {
+                    count += 1;
+                    if (count == multiples) {
+                        multiplied = true;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(num);
     }
 }
