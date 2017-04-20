1ffc4766d5735ea1693606a068a285e0760be332
jphillips2121 <jphillips3@companieshouse.gov.uk>
2017-04-20 11:08:55 +0100

    Sum of multiples of 3 and 5

--- Problem-1/src/Multiples3and5.java
+++ Problem-1/src/Multiples3and5.java
@@ -4,6 +4,7 @@
 public class Multiples3and5 {
         public static void main(String[] args) {
             int count = 0;
+            //Loops 1 to 1000 
             for (int i = 1; i < 1000; i++) {
                 if ((i % 3 == 0) || (i % 5 == 0)) {
                     count += i;
